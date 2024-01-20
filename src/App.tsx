2b97f770179c9a71ee0f613a714db0e1a0ff1104
Douglas Souza <lyricsan@hotmail.com>
2024-01-20 12:46:27 -0300

    exibindo rota principal/header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div className="w-screen h-screen bg-zinc-800 text-white">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
