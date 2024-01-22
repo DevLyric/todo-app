56d4a14d6f9787a6662653e4b4f011ada963b0e2
Douglas Souza <lyricsan@hotmail.com>
2024-01-22 12:26:04 -0300

    condição para exibir logout ou start for free

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,44 @@
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { logout } from "../constants";
 
 export default function Header() {
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+
+  const auth = getAuth();
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      setIsLoggedIn(true);
+      const uid = user.uid;
+      console.log(uid);
+    } else {
+      setIsLoggedIn(false);
+    }
+  });
+
   return (
     <header className="w-full h-20 flex items-center">
       <div className="container mx-auto max-w-8xl flex items-center justify-between px-6">
         <h1 className="font-semibold text-xl">Todo</h1>
-        <Link
-          to="/auth/signup"
-          className="p-2 font-medium text-sm rounded bg-[#de483a]"
-        >
-          Comece de graça
-        </Link>
+
+        {isLoggedIn ? (
+          <button
+            onClick={() => {
+              logout();
+              window.location.href = "/auth/login";
+            }}
+          >
+            logout
+          </button>
+        ) : (
+          <Link
+            to="/auth/signup"
+            className="p-2 font-medium text-sm rounded bg-[#de483a]"
+          >
+            start for free
+          </Link>
+        )}
       </div>
     </header>
   );
